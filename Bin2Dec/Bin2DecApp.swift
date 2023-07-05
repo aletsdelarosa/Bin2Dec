267e7efef0b440510d521e3e7357abf56c104de4
Alex de la Rosa <alexdelarosacortes@gmail.com>
2023-07-05 10:36:10 -0600

    Initial Commit

---
+++ Bin2Dec/Bin2DecApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bin2DecApp.swift
+//  Bin2Dec
+//
+//  Created by Alex de la Rosa on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Bin2DecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
